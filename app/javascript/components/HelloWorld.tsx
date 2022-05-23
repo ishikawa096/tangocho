d283614edb9da9e84ec516d41508259a27825d87
ishikawa096 <ishikawa096@gmail.com>
2022-05-24 00:14:49 +0900

    typescript導入

---
+++ app/javascript/components/HelloWorld.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+type Props = {
+  greeting: string
+}
+
+const HelloWorld: React.VFC<Props> = ({ greeting }) => {
+  return <>
+    Greetingts: {greeting}
+  </>
+}
+
+export default HelloWorld
