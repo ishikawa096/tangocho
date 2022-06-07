ddbcf3f70fe78dde48f89814242a6c214a88230f
ishikawa096 <ishikawa096@gmail.com>
2022-06-07 19:12:07 +0900

    仮test修正

--- spec/system/cards_spec.rb
+++ spec/system/cards_spec.rb
@@ -4,6 +4,6 @@ WebMock.disable_net_connect!(allow: ['web', 'chrome:4444'])
 RSpec.describe 'react', type: :system, js: true do
   it 'react' do
     visit root_path
-    expect(page).to have_content('Hello, World!')
+    expect(page).to have_content('Tangocho')
   end
 end
