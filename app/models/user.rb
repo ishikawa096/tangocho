ad37f1ec152ad2837e4a8f02ca589a3a270f20fb
ishikawa096 <ishikawa096@gmail.com>
2022-06-18 09:58:24 +0900

    model等新規ファイル作成、不要ファイル削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :trackable, :validatable,
-    :confirmable, :omniauthable
+    :recoverable, :rememberable, :validatable # , :trackable,
+    # :confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
+  has_many :cards, dependent: :destroy
+  has_many :books, dependent: :destroy
 end
